3c4bfac6a1c200438a01ced4571e1808ba6a1ba1
Locki Lo <locki.lo@cargosmart.com>
2018-11-15 16:56:06 +0800

    Completed Test04, modified introduce() in Worker & Student

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,27 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int klass){
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public String getName() {
+        return super.getName();
+    }
+
+    public int getAge() {
+        return super.getAge();
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        //I am a Student. I am at Class 2.
+        return super.introduce().trim() + String.format("I am a Student. I am at Class %d.", this.klass);
+    }
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,21 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+
+    public Worker(String name, int age){
+        super(name, age);
+    }
+
+    public String getName() {
+        return super.getName();
+    }
+
+    public int getAge() {
+        return super.getAge();
+    }
+
+    public String introduce(){
+        //I am a Worker. I have a job.
+        return super.introduce().trim() +"I am a Worker. I have a job.";
+    }
 }
