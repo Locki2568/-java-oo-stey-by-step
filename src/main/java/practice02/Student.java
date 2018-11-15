0fb1aa67762e5b3d2deeb96a7b9abe8f1029c780
Locki Lo <locki.lo@cargosmart.com>
2018-11-15 16:48:41 +0800

    Complted Person, Student, Worker Class and Test

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,27 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int klass){
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public String getName() {
+        return super.getName();
+    }
+
+    public int getAge() {
+        return super.getAge();
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        //I am a Student. I am at Class 2.
+        return String.format("I am a Student. I am at Class %d.", this.klass);
+    }
 }
