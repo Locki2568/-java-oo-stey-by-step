2cf167aaed3eb60f89c9e3fd8c3cd24d40327f15
Locki Lo <locki.lo@cargosmart.com>
2018-11-16 10:25:57 +0800

    Completed Test10, added LinkedLikn and modified introduce() & introduceWith() in Teacher, added isIn() in Student.

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,63 @@
 package practice10;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private int klass;
+    private Teacher teacherInKlass;
+    private ArrayList<Student> studentsListInKlass;
+    private Student leader;
+
+    public Klass(Teacher teacher, int klass){
+        this.teacherInKlass = teacher;
+        this.klass = klass;
+    }
+
+    public Klass(Student student, int klass){
+        //addStudentToList(student);
+        this.klass = klass;
+    }
+
+    public Klass(int klass){
+        this.studentsListInKlass = new ArrayList<>();
+        this.klass = klass;
+    }
+
+    public Klass(){
+    }
+
+    public int getNumber(){
+        return this.klass;
+    }
+
+    public int getKlass(){
+        return this.klass;
+    }
+
+    public String getDisplayName(){
+        if(this.klass != 0){
+            return "Class "+this.klass;
+        }else{
+            return "No Class";
+        }
+    }
+
+    public void appendMember(Student student){
+        this.studentsListInKlass.add(student);
+    }
+
+    public void assignLeader(Student jerry) {
+        if (!studentsListInKlass.contains(jerry)){
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        this.leader = jerry;
+    }
+
+    public Student getLeader() {
+        if(leader != null) {
+            return this.leader;
+        }
+        return null;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,73 @@
 package practice10;
 
-public class Teacher {
+import sun.awt.image.ImageWatched;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    private Klass klass;
+    LinkedList<Klass> linkedList;
+
+    public Teacher(int id, String name, int age, Klass klass){
+        super(id,name,age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age){
+        super(id,name,age);
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> linkedList){
+        super(id,name,age);
+        this.linkedList = linkedList;
+    }
+
+    public Klass getKlass(){return  this.klass;}
+
+    public String getName() {
+        return super.getName();
+    }
+
+    public int getAge() {
+        return super.getAge();
+    }
+
+    public String introduce(){
+        //"My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2, 3."
+        return super.introduce() + " " + String.format("I am a Teacher. I teach %s.", getDisplayNameFromList());
+    }
+
+    private String getDisplayNameFromList(){
+        if (linkedList == null){
+            return "No Class";
+        }
+        String displayName = "Class ";
+        for(Klass klass :this.linkedList){
+            displayName += klass.getNumber() + ", ";
+        }
+        return displayName.substring(0, displayName.length()-2);
+    }
+
+    public String introduceWith(Student student) {
+        for (Klass klass : linkedList) {
+            if (student.isIn(klass)) {
+                return super.introduce() + " " + String.format("I am a Teacher. I teach %s.", student.getName());
+            }
+        }
+        return super.introduce() + " " + String.format("I am a Teacher. I don't teach %s.", student.getName());
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return linkedList;
+    }
+
+    public boolean isTeaching(Student student) {
+        for(Klass klass: linkedList){
+            if (student.isIn(klass)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
